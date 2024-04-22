383dad7a8f0c54d1dd5cc96dca8e9cfaab6424a5
clair234 <clairhany2017@gmail.com>
2024-04-21 17:16:37 -0700

    route3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,3 +23,26 @@ def hello_hbnb():
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
